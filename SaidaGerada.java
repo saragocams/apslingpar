7c52a0dcc72939c52138c6ac32abdf4ac5eb5a14
saragocams <saragocams@gmail.com>
2025-05-19 15:33:21 -0300

    adicionando base de testes

--- SaidaGerada.java
+++ SaidaGerada.java
@@ -1,13 +1,7 @@
 public class SaidaGerada {
   public static void main(String[] args) {
-System.out.println("Invocando corvo_negro com poder 13");
-if (7 > 10) {
-System.out.println("O reino caira em sono eterno...");
-System.out.println("Invocando espinhos_venenosos com poder 77");
-}
-for (int i = 0; i < 3; i++) {
-System.out.println("A magia cresce com cada batida de asa...");
-System.out.println("Invocando fogo_verde com poder 99");
+for (int i = 0; i < 1; i++) {
+System.out.println("O ciclo começou com um sussurro...");
 }
   }
 }
